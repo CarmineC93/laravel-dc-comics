8955f46b5f1f9a5182707a0c61d6835bab5891b3
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-23 15:28:53 +0100

    add some comments

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -9,5 +9,5 @@ class Comic extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
-    //necessario per l'edit e l'update
+    //necessario per l'edit e l'update, deve autorizzare ogni cambiamento e iniezione di codice solamente sui campi
 }
